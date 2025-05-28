647dca9a857332e8af8403122878c931f748bc95
n3w6y <119931002+n3w6y@users.noreply.github.com>
2025-05-28 20:44:17 +1000

    Update conclusion.dart

--- lib/src/service/tweets/filtering/syntax/conclusion.dart
+++ lib/src/service/tweets/filtering/syntax/conclusion.dart
@@ -1,11 +1,5 @@
-// Copyright 2022 Kato Shinya. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided the conditions.
-
-// 🌎 Project imports:
 import 'filtering_rule_syntax.dart';
 
-abstract class Conclusion implements FilteringRuleSyntax {
-  /// Returns the built filtering rule.
-  String build();
+class Conclusion extends FilteringRuleSyntax {
+  Conclusion({required super.initialRule});
 }
