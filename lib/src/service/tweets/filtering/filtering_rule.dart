7a71503bca9eacae9d1147b8b0871d3c4d6c6db0
n3w6y <119931002+n3w6y@users.noreply.github.com>
2025-05-28 19:10:49 +1000

    Filtering_Rule and uploaded_media_data file updates

--- lib/src/service/tweets/filtering/filtering_rule.dart
+++ lib/src/service/tweets/filtering/filtering_rule.dart
@@ -2,18 +2,35 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
-// 🌎 Project imports:
-import 'channel/entry_channel.dart';
-import 'filtering_rule_group.dart';
+import 'package:twitter_api_v2/src/core/client/client_context.dart';
+import 'package:twitter_api_v2/src/service/response/twitter_response.dart';
+import 'package:twitter_api_v2/src/service/tweets/filtering_rule_data.dart';
+import 'package:twitter_api_v2/src/service/tweets/filtering/filtering_rule_group.dart';
 
 class FilteringRule {
-  /// Returns the new instance of [FilteringRule].
-  FilteringRule._();
+  FilteringRule({
+    required ClientContext context,
+    required this.value,
+    this.id,
+    this.tag,
+  }) : _filteringRuleGroup = FilteringRuleGroup(context: context);
 
-  /// Returns the new entry point to build filtering rules.
-  static EntryChannel of() => FilteringRuleGroup().entryChannel;
+  final String value;
+  final String? id;
+  final String? tag;
+  final FilteringRuleGroup _filteringRuleGroup;
 
-  /// Returns the new entry point to build filtering rules with sample percent.
-  static EntryChannel ofSample({required int percent}) =>
-      FilteringRuleGroup(percent).entryChannel;
+  Future<TwitterResponse<List<FilteringRuleData>, void>> create() async {
+    return await _filteringRuleGroup.create(
+      value: value,
+      tag: tag,
+    );
+  }
+
+  Future<TwitterResponse<List<FilteringRuleData>, void>> delete() async {
+    if (id == null) {
+      throw StateError('Cannot delete a rule without an ID');
+    }
+    return await _filteringRuleGroup.deleteRules(ruleIds: [id!]);
+  }
 }
