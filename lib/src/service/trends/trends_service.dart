69b4b01604ca50bd1a2f65a5c4bdb45390c9de98
n3w6y <119931002+n3w6y@users.noreply.github.com>
2025-05-28 20:52:10 +1000

    Update trends_service.dart

--- lib/src/service/trends/trends_service.dart
+++ lib/src/service/trends/trends_service.dart
@@ -3,23 +3,13 @@ import 'package:twitter_api_v2/src/core/client/user_context.dart';
 import 'package:twitter_api_v2/src/service/base_service.dart';
 import 'package:twitter_api_v2/src/service/response/twitter_response.dart';
 import 'package:twitter_api_v2/src/service/trends/trend_data.dart';
-import 'package:twitter_api_v2/src/service/trends/trending_location_data.dart';
 
 abstract class TrendsService {
   factory TrendsService({required ClientContext context}) =>
       _TrendsService(context: context);
 
-  Future<TwitterResponse<List<TrendingLocationData>, void>>
-      lookupAvailableLocations();
-
-  Future<TwitterResponse<List<TrendData>, void>> lookupByWoeId({
-    required int woeId,
-  });
-
-  Future<TwitterResponse<List<TrendingLocationData>, void>>
-      lookupClosestLocations({
-    required double latitude,
-    required double longitude,
+  Future<TwitterResponse<List<TrendData>, void>> lookupByWoeid({
+    required int woeid,
   });
 }
 
@@ -27,48 +17,30 @@ class _TrendsService extends BaseService implements TrendsService {
   _TrendsService({required super.context});
 
   @override
-  Future<TwitterResponse<List<TrendingLocationData>, void>>
-      lookupAvailableLocations() async {
-    return await super.get(
-      context,
-      '/1.1/trends/available.json',
-      fromJsonData: (json) =>
-          (json as List).map((e) => TrendingLocationData.fromJson(e)).toList(),
-      userContext: UserContext.oauth2OrOAuth1,
-    );
-  }
-
-  @override
-  Future<TwitterResponse<List<TrendData>, void>> lookupByWoeId({
-    required int woeId,
+  Future<TwitterResponse<List<TrendData>, void>> lookupByWoeid({
+    required int woeid,
   }) async {
-    return await super.get(
+    if (!_validWoeids.contains(woeid.toString())) {
+      throw ArgumentError('The specified WOEID is not available.');
+    }
+
+    final response = await super.get(
       context,
-      '/1.1/trends/place.json',
-      fromJsonData: (json) => (json[0]['trends'] as List)
-          .map((e) => TrendData.fromJson(e))
+      '/2/trends/by/woeid',
+      queryParameters: {'woeid': woeid},
+      userContext: UserContext.oauth2Only,
+      fromJsonData: (json) => (json['trends'] as List<dynamic>)
+          .map((trend) => TrendData.fromJson(trend))
           .toList(),
-      queryParameters: {'id': woeId},
-      userContext: UserContext.oauth2OrOAuth1,
     );
+
+    return response;
   }
 
-  @override
-  Future<TwitterResponse<List<TrendingLocationData>, void>>
-      lookupClosestLocations({
-    required double latitude,
-    required double longitude,
-  }) async {
-    return await super.get(
-      context,
-      '/1.1/trends/closest.json',
-      fromJsonData: (json) =>
-          (json as List).map((e) => TrendingLocationData.fromJson(e)).toList(),
-      queryParameters: {
-        'lat': latitude,
-        'long': longitude,
-      },
-      userContext: UserContext.oauth2OrOAuth1,
-    );
-  }
+  static const _validWoeids = [
+    '1',
+    '23424747',
+    '23424748',
+    // ... other WOEIDs
+  ];
 }
