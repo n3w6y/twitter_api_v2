7d969bc07ea37105d524a8cbb2b66cb86adaa1ab
n3w6y <119931002+n3w6y@users.noreply.github.com>
2025-05-28 14:46:15 +1000

    Update base_media_service.dart

--- lib/src/service/base_media_service.dart
+++ lib/src/service/base_media_service.dart
@@ -1,5 +1,3 @@
-import 'package:twitter_api_v2/src/core/client/client_context.dart';
-import 'package:twitter_api_v2/src/core/client/user_context.dart';
 import 'package:twitter_api_v2/src/service/base_service.dart';
 
 abstract class BaseMediaService extends BaseService {
