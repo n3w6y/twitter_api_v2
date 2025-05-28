d8ad1d06a32bd1bd32cb715d5dac2b285c39eb0b
n3w6y <119931002+n3w6y@users.noreply.github.com>
2025-05-28 19:57:37 +1000

    Update entry_channel.dart

--- lib/src/service/tweets/filtering/channel/entry_channel.dart
+++ lib/src/service/tweets/filtering/channel/entry_channel.dart
@@ -1,12 +1,19 @@
-// Copyright 2022 Kato Shinya. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided the conditions.
+import 'package:twitter_api_v2/src/core/client/client_context.dart';
+import 'package:twitter_api_v2/src/service/tweets/filtering/filtering_rule.dart';
+import 'package:twitter_api_v2/src/service/tweets/filtering/syntax/filtering_rule_syntax.dart';
 
-// 🌎 Project imports:
-import '../syntax/standalone_syntax.dart';
-import 'filtering_rule_channel.dart';
+class EntryChannel extends FilteringRuleSyntax {
+  EntryChannel({super.initialRule = ''});
 
-class EntryChannel extends StandaloneSyntax implements FilteringRuleChannel {
-  /// Returns the new instance of [EntryChannel].
-  const EntryChannel(super.buffer);
+  @override
+  String build() => super.build();
+
+  @override
+  FilteringRule buildRule({required ClientContext context, String? tag}) {
+    return FilteringRule(
+      context: context,
+      value: build(),
+      tag: tag,
+    );
+  }
 }
