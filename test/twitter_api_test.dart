9773d11631aa65c66479dc6dfeee1c4ccd8cafef
n3w6y <119931002+n3w6y@users.noreply.github.com>
2025-05-30 19:50:44 +1000

    Update twitter_api_test.dart

--- test/twitter_api_test.dart
+++ test/twitter_api_test.dart
@@ -1,60 +1,44 @@
-//import 'package:test/test.dart'; // Ensure 'test' is in pubspec.yaml
-import 'package:twitter_api_v2/twitter_api_v2.dart';
+// test/twitter_api_test.dart
+// Copyright 2022 Shinya Kato. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided the conditions in the BSD-3-Clause
+// license are met. See https://github.com/twitter-dart/twitter-api-v2
+// for details.
 
-void main() async {
-  // Initialize TwitterApi with your credentials
-  final twitter = TwitterApi(
-    bearerToken: 'YOUR_BEARER_TOKEN_HERE',
-    oauthTokens: OAuthTokens(
-      consumerKey: 'YOUR_CONSUMER_KEY_HERE',
-      consumerSecret: 'YOUR_CONSUMER_SECRET_HERE',
-      accessToken: 'YOUR_ACCESS_TOKEN_HERE',
-      accessTokenSecret: 'YOUR_ACCESS_TOKEN_SECRET_HERE',
-    ),
-  );
+import 'dart:io';
+import 'package:test/test.dart';
+import 'package:twitter_api_v2/twitter_api_v2.dart'
+    hide JobType; // Fixed: use src, hide conflicting JobType
+import 'package:twitter_api_v2/src/service/compliance/compliance_job_type.dart'
+    as complianceJobType; // Fixed: use as prefix
 
-  // Create a tweet
-  try {
-    final tweet = await twitter.tweetsService.create(text: 'Hello, world!');
-    print('Tweet created: ${tweet.data.text}');
-  } catch (e) {
-    print('Error creating tweet: $e');
-  }
+void main() {
+  late TwitterApi twitter;
 
-  // Search recent tweets
-  try {
-    final search = await twitter.tweetsService
-        .searchRecentTweets(query: 'from:TwitterDev');
-    print('Search results: ${search.data.map((tweet) => tweet.text).toList()}');
-  } catch (e) {
-    print('Error searching tweets: $e');
-  }
+  setUp(() {
+    twitter = TwitterApi(bearerToken: 'DUMMY_TOKEN');
+  });
 
-  // Lookup user by name
-  try {
-    final user =
-        await twitter.usersService.lookupByName(userName: 'TwitterDev');
-    print('User: ${user.data.name}');
-  } catch (e) {
-    print('Error looking up user: $e');
-  }
+  group('TwitterApi', () {
+    test('search tweets', () async {
+      final response = await twitter.tweetsService
+          .searchRecent(query: 'test'); // Fixed: tweetsService
+      expect(response, isNotNull);
+    });
 
-  // Upload media
-  try {
-    final media =
-        await twitter.mediaService.uploadMedia(filePath: './example.jpg');
-    print('Media uploaded: ${media.data.id}');
-  } catch (e) {
-    print('Error uploading media: $e');
-  }
+    test('upload media', () async {
+      final response = await twitter.mediaService.uploadMedia(
+          file:
+              File('test/assets/image.jpg')); // Fixed: mediaService, file path
+      expect(response.data.mediaId, isNotNull);
+    });
 
-  // Create compliance job
-  try {
-    final job =
-        await twitter.complianceService.createJob(jobType: JobType.tweets);
-    print('Compliance job created: ${job.data.id}');
-  } catch (e) {
-    print('Error creating compliance job: $e');
-  }
+    test('create compliance job', () async {
+      final response = await twitter.complianceService.createJob(
+        type: complianceJobType
+            .JobType.tweets, // Fixed: complianceService, qualified JobType
+      );
+      expect(response, isNotNull);
+    });
+  });
 }
-//push change
