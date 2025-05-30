6e8f5a72c9c8081615cdf17e9eb56a11b92ad44a
n3w6y <119931002+n3w6y@users.noreply.github.com>
2025-05-30 19:58:35 +1000

    Update twitter_api_test.dart

--- test/twitter_api_test.dart
+++ test/twitter_api_test.dart
@@ -1,4 +1,3 @@
-// test/twitter_api_test.dart
 // Copyright 2022 Shinya Kato. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions in the BSD-3-Clause
@@ -8,9 +7,9 @@
 import 'dart:io';
 import 'package:test/test.dart';
 import 'package:twitter_api_v2/twitter_api_v2.dart'
-    hide JobType; // Fixed: use src, hide conflicting JobType
+    hide JobType; // Correct path, hide conflicting JobType
 import 'package:twitter_api_v2/src/service/compliance/compliance_job_type.dart'
-    as complianceJobType; // Fixed: use as prefix
+    as compliance_job_type; // Compliant prefix
 
 void main() {
   late TwitterApi twitter;
@@ -22,21 +21,19 @@ void main() {
   group('TwitterApi', () {
     test('search tweets', () async {
       final response = await twitter.tweetsService
-          .searchRecent(query: 'test'); // Fixed: tweetsService
+          .lookupByQuery(query: 'test'); // Fixed: lookupByQuery
       expect(response, isNotNull);
     });
 
     test('upload media', () async {
-      final response = await twitter.mediaService.uploadMedia(
-          file:
-              File('test/assets/image.jpg')); // Fixed: mediaService, file path
-      expect(response.data.mediaId, isNotNull);
+      final response = await twitter.mediaService
+          .uploadMedia(file: File('test/assets/image.jpg')); // Fixed path
+      expect(response.data.media_id, isNotNull); // Fixed: media_id
     });
 
     test('create compliance job', () async {
       final response = await twitter.complianceService.createJob(
-        type: complianceJobType
-            .JobType.tweets, // Fixed: complianceService, qualified JobType
+        jobType: compliance_job_type.JobType.tweets, // Fixed: jobType
       );
       expect(response, isNotNull);
     });
