bb70737028182c14f045e745997102049a186ea8
n3w6y <119931002+n3w6y@users.noreply.github.com>
2025-06-04 15:28:11 +1000

    Update tweets_service_extension.dart

--- lib/src/service/tweets/tweets_service_extension.dart
+++ lib/src/service/tweets/tweets_service_extension.dart
@@ -1,41 +1,43 @@
+import 'package:twitter_api_v2/src/service/tweets/tweets_service.dart';
+import 'package:twitter_api_v2/src/service/tweets/tweet_param.dart';
 import 'package:twitter_api_v2/src/service/response/twitter_response.dart';
 import 'package:twitter_api_v2/src/service/tweets/tweet_data.dart';
-import 'package:twitter_api_v2/src/service/tweets/tweets_service.dart';
+//import 'package:twitter_api_v2/src/service/tweets/tweet_media_param.dart';
 
 extension TweetsServiceExtension on TweetsService {
-  /// A synonym for [create] method.
-  ///
-  /// ## Parameters
-  ///
-  /// - [text]: The text of the Tweet being created.
-  ///           Maximum of 280 characters.
-  ///
-  /// ## Returns
-  ///
-  /// A [Future] that returns a [TwitterResponse] containing the created [TweetData].
-  Future<TwitterResponse<TweetData, void>> post({
+  Future<TwitterResponse<TweetData, void>> createReply({
+    required String tweetId,
     required String text,
-  }) async =>
-      create(
-        text: text,
-      );
+  }) async {
+    if (tweetId.isEmpty) {
+      throw ArgumentError('The Tweet ID is required to create a reply.');
+    }
+    return await create(
+      text: text,
+      replyToTweetId: tweetId,
+    );
+  }
 
-  /// A synonym for [create] method.
-  ///
-  /// This method is an alias of [post] method.
-  ///
-  /// ## Parameters
-  ///
-  /// - [text]: The text of the Tweet being created.
-  ///           Maximum of 280 characters.
-  ///
-  /// ## Returns
-  ///
-  /// A [Future] that returns a [TwitterResponse] containing the created [TweetData].
-  Future<TwitterResponse<TweetData, void>> tweet({
-    required String text,
-  }) async =>
-      create(
-        text: text,
+  Future<List<TwitterResponse<TweetData, void>>> createThreads({
+    required List<TweetParam> tweets,
+  }) async {
+    if (tweets.length < 2) {
+      throw ArgumentError(
+        'At least 2 tweet datasets are required to create a threaded tweet.',
       );
+    }
+    final responses = <TwitterResponse<TweetData, void>>[];
+    TwitterResponse<TweetData, void>? previous;
+    for (var i = 0; i < tweets.length; i++) {
+      final param = tweets[i];
+      final response = await create(
+        text: param.text,
+        replyToTweetId: previous?.data.id,
+        mediaIds: param.media?.mediaIds,
+      );
+      responses.add(response);
+      previous = response;
+    }
+    return responses;
+  }
 }
